56f3e7a638bfd4bfdb9bb0ddd07af3de616fc1b3
Ashutosh giri <123761816+Ashutosh975537@users.noreply.github.com>
2024-07-07 08:36:13 +0530

    Create 07-07-2024.cpp

---
+++ 07-07-2024.cpp
@@ -0,0 +1,33 @@
+/*
+struct Node 
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    struct Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+        next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* pre = NULL ;
+    void populateNext(Node *root) {
+        // code here
+        if(root == NULL)return ;
+        populateNext(root -> left);
+        if(pre == NULL)
+           pre = root ;
+        else{
+            pre -> next = root ;
+            pre = root ;
+        }
+        populateNext(root -> right);
+    }
+};
